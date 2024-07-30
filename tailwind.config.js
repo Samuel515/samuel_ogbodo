34351f865a0e4ff9bf35f375b9e92b178882d658
Samuel515 <samuelogbodo515@gmail.com>
2024-07-30 17:00:50 +0100

    restarted with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#282C33",
+        primary: "#008000",
+        gray: "#ABB2BF"
+      },
+      fontFamily: {
+        firacode: ["Fira Code" , "sans serif"]
+      },
+      content: {
+        'image-before': 'url("../assets/bg.svg")',
+        'image-after': 'url("../assets/dots.svg")',
+      },
+    },
+  },
+  plugins: [],
+}
